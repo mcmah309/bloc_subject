9bf84524c5664d99821008cb86e3318b8249d824
mcmah309 <dillonmcmahon79@gmail.com>
2024-09-17 15:27:55 +0000

    feat: Save bloc subject poc

---
+++ lib/src/bloc_subject_poc.dart
@@ -0,0 +1,508 @@
+import 'dart:async';
+
+import 'package:bloc_rxdart/bloc_rxdart.dart';
+import 'package:bloc_rxdart/src/exceptions.dart';
+import 'package:rxdart/src/streams/value_stream.dart';
+import 'package:rxdart/src/utils/notification.dart';
+import 'package:rxdart/subjects.dart';
+import 'package:rxdart/transformers.dart';
+
+/// Handler function for events when the state exists
+typedef HandlerFn<Event, State> = FutureOr<State?> Function(Event, State);
+
+/// In some cases it is possible for an [Event] to be received before the first [State] is set.
+/// This is the callback for such cases
+/// To avoid this, when needed, consider using [waitForState] before adding any [Event]s.
+typedef FallbackFn<Event, State> = FutureOr<State?> Function(Event);
+
+/// All types of event handlers
+sealed class EventHandler<Event, State> {
+  static OptDiffHandler<Event, State> optDiff<Event, State>(
+          {required HandlerFn<Event, State> handler, required FallbackFn<Event, State> fallback}) =>
+      OptDiffHandler(handler: handler, fallback: fallback);
+
+  static OptSameHandler<Event, State> optSame<Event, State>(HandlerFn<Event, State?> handler) =>
+      OptSameHandler(handler);
+
+  static ExistsHandler<Event, State> exists<Event, State>(HandlerFn<Event, State> handler) =>
+      ExistsHandler(handler);
+}
+
+/// Event handlers for when the state may not exist
+sealed class OptEventHandler<Event, State> implements EventHandler<Event, State> {
+  static OptDiffHandler<Event, State> diff<Event, State>(
+          {required HandlerFn<Event, State> handler, required FallbackFn<Event, State> fallback}) =>
+      OptDiffHandler(handler: handler, fallback: fallback);
+
+  static OptSameHandler<Event, State> same<Event, State>(HandlerFn<Event, State?> handler) =>
+      OptSameHandler(handler);
+}
+
+/// Event handler when the state may not exist and the functionality is different when the state exists vs not.
+class OptDiffHandler<Event, State> implements OptEventHandler<Event, State> {
+  final HandlerFn<Event, State> handler;
+  final FallbackFn<Event, State> fallback;
+
+  OptDiffHandler({required this.handler, required this.fallback});
+
+  FutureOr<State?> handle(Event event, State? state) {
+    if (state == null) {
+      return fallback(event);
+    }
+    return handler(event, state);
+  }
+}
+
+/// Event handler when the state may not exist and functionality is the same when the state exists vs not.
+class OptSameHandler<Event, State> implements OptEventHandler<Event, State> {
+  final HandlerFn<Event, State?> handler;
+
+  OptSameHandler(this.handler);
+
+  FutureOr<State?> handle(Event event, State? state) {
+    return handler(event, state);
+  }
+}
+
+/// Event handler when it is known the state exists.
+class ExistsHandler<Event, State> implements EventHandler<Event, State> {
+  final HandlerFn<Event, State> handler;
+
+  ExistsHandler(this.handler);
+
+  FutureOr<State?> handle(Event event, State state) {
+    return handler(event, state);
+  }
+}
+
+class BlocSubject<Event, State> implements BehaviorSubject<State> {
+  /// Useful if you want to set logging.
+  static FallbackFn? defaultEmptyHandler;
+
+  final BehaviorSubject<State> _states; // in/out
+  final BehaviorSubject<Event> _events = BehaviorSubject(); // in, all events get added here
+  final Set<StreamSubscription<Event>> _additionalEventSubscriptions = {};
+  StreamSubscription<State>? _transformSubscription;
+
+  BlocSubject._(this._states, EventHandler<Event, State> handler) {
+    _eventHandler(handler);
+  }
+
+  factory BlocSubject({
+    required OptEventHandler<Event, State> handler,
+    void Function()? onListen,
+    void Function()? onCancel,
+    bool sync = false,
+  }) {
+    final BehaviorSubject<State> behavior =
+        BehaviorSubject(onListen: onListen, onCancel: onCancel, sync: sync);
+    return BlocSubject._(behavior, handler);
+  }
+
+  factory BlocSubject.fromStream(
+    Stream<State> stream, {
+    required OptEventHandler<Event, State> handler,
+    void Function()? onListen,
+    void Function()? onCancel,
+    bool sync = false,
+  }) {
+    final BehaviorSubject<State> behavior =
+        BehaviorSubject(onListen: onListen, onCancel: onCancel, sync: sync);
+    behavior.addStream(stream);
+    return BlocSubject._(behavior, handler);
+  }
+
+  factory BlocSubject.fromBehavior(
+    BehaviorSubject<State> behavior, {
+    required OptEventHandler<Event, State> handler,
+    FallbackFn<Event, State>? emptyHandler,
+  }) {
+    return BlocSubject._(behavior, handler);
+  }
+
+  factory BlocSubject.fromValue(
+    State val, {
+    required ExistsHandler<Event, State> eventHandler,
+    void Function()? onListen,
+    void Function()? onCancel,
+    bool sync = false,
+  }) {
+    final BehaviorSubject<State> behavior =
+        BehaviorSubject.seeded(val, onListen: onListen, onCancel: onCancel, sync: sync);
+    return BlocSubject._(behavior, eventHandler);
+  }
+
+  //************************************************************************//
+
+  void _eventHandler(EventHandler<Event, State> handler) {
+    if (_transformSubscription != null) {
+      _transformSubscription!.cancel();
+    }
+    final transform = _events.flatMap<State>((event) async* {
+      final state = _states.valueOrNull;
+      final State? newData;
+      switch (handler) {
+        case ExistsHandler<Event, State>():
+          assert(state is State,
+              "Internal logic error, this should never happen. Distinct can only be used provided to the api when it is known to be distinct");
+          newData = await handler.handle(event, state as State);
+        case OptEventHandler<Event, State>():
+          switch (handler) {
+            case OptDiffHandler<Event, State>():
+              newData = await handler.handle(event, state);
+            case OptSameHandler<Event, State>():
+              newData = await handler.handle(event, state);
+          }
+      }
+      if (newData == null) {
+        return;
+      }
+      yield newData;
+    });
+    _transformSubscription = transform.listen((data) {
+      _states.add(data);
+    }, onError: (Object error, StackTrace stackTrace) {
+      _states.addError(error, stackTrace);
+    }, onDone: () {
+      _states.close();
+    });
+  }
+
+  /// Remits the last emitted value
+  void reEmit() {
+    if (isClosed) {
+      throw const SubjectClosed();
+    }
+    final val = _states.value;
+    _states.add(val);
+  }
+
+  void listenToEvents(Stream<Event> events) {
+    if (isClosed) {
+      throw const SubjectClosed();
+    }
+    final sub = events.listen((event) {
+      addEvent(event);
+    });
+    _additionalEventSubscriptions.add(sub);
+  }
+
+  /// Adds an event. Note an event is not immediately processed. You need to yield to the scheduler since
+  /// `handleEvents` functions is allowed to be async.
+  void addEvent(Event event) {
+    if (isClosed) {
+      throw const SubjectClosed();
+    }
+    _events.add(event);
+  }
+
+  /// Waits for the state to exist or for the optionally given [timeout]. Returns `true` if the state exists or
+  /// `false` if the state does not exist within the timeout.
+  Future<bool> waitForState([Duration? timeout]) async {
+    if (_states.hasValue) {
+      return true;
+    }
+    if (timeout == null) {
+      return _states.first.then((v) => true);
+    }
+    try {
+      await _states.first.timeout(timeout);
+      return true;
+    } on TimeoutException catch (_) {
+      return false;
+    }
+  }
+
+  // BehaviorSubject
+  //************************************************************************//
+
+  @override
+  ControllerCancelCallback? get onCancel => _states.onCancel;
+
+  @override
+  ControllerCallback? get onListen => _states.onListen;
+
+  @override
+  ControllerCallback get onPause => _states.onPause;
+
+  @override
+  ControllerCallback get onResume => _states.onResume;
+
+  @override
+  State get value => _states.value;
+
+  @override
+  set onCancel(ControllerCancelCallback? onCancelHandler) {
+    _states.onCancel = onCancelHandler;
+  }
+
+  @override
+  set onListen(void Function()? onListenHandler) {
+    _states.onListen = onListenHandler;
+  }
+
+  @override
+  set onPause(void Function()? onPauseHandler) {
+    _states.onPause = onPauseHandler;
+  }
+
+  @override
+  set onResume(void Function()? onResumeHandler) {
+    _states.onResume = onResumeHandler;
+  }
+
+  @override
+  set value(State newValue) {
+    _states.value = newValue;
+  }
+
+  @override
+  void add(State event) {
+    _states.add(event);
+  }
+
+  @override
+  void addError(Object error, [StackTrace? stackTrace]) {
+    _states.addError(error, stackTrace);
+  }
+
+  @override
+  Future<void> addStream(Stream<State> source, {bool? cancelOnError}) {
+    return _states.addStream(source, cancelOnError: cancelOnError);
+  }
+
+  @override
+  Future<bool> any(bool Function(State element) test) {
+    return _states.any(test);
+  }
+
+  @override
+  Stream<State> asBroadcastStream(
+      {void Function(StreamSubscription<State> subscription)? onListen,
+      void Function(StreamSubscription<State> subscription)? onCancel}) {
+    return _states.asBroadcastStream(onListen: onListen, onCancel: onCancel);
+  }
+
+  @override
+  Stream<E> asyncExpand<E>(Stream<E>? Function(State event) convert) {
+    return _states.asyncExpand(convert);
+  }
+
+  @override
+  Stream<E> asyncMap<E>(FutureOr<E> Function(State event) convert) {
+    return _states.asyncMap(convert);
+  }
+
+  @override
+  Stream<R> cast<R>() {
+    return _states.cast();
+  }
+
+  @override
+  Future close() {
+    _transformSubscription?.cancel();
+    _events.close();
+    for (final sub in _additionalEventSubscriptions) {
+      sub.cancel();
+    }
+    return _states.close();
+  }
+
+  @override
+  Future<bool> contains(Object? needle) {
+    return _states.contains(needle);
+  }
+
+  @override
+  Stream<State> distinct([bool Function(State previous, State next)? equals]) {
+    return _states.distinct(equals);
+  }
+
+  @override
+  Future get done => _states.done;
+
+  @override
+  Future<E> drain<E>([E? futureValue]) {
+    return _states.drain(futureValue);
+  }
+
+  @override
+  Future<State> elementAt(int index) {
+    return _states.elementAt(index);
+  }
+
+  @override
+  Object get error => _states.error;
+
+  @override
+  Object? get errorOrNull => _states.errorOrNull;
+
+  @override
+  Future<bool> every(bool Function(State element) test) {
+    return _states.every(test);
+  }
+
+  @override
+  Stream<S> expand<S>(Iterable<S> Function(State element) convert) {
+    return _states.expand(convert);
+  }
+
+  @override
+  Future<State> get first => _states.first;
+
+  @override
+  Future<State> firstWhere(bool Function(State element) test, {State Function()? orElse}) {
+    return _states.firstWhere(test, orElse: orElse);
+  }
+
+  @override
+  Future<S> fold<S>(S initialValue, S Function(S previous, State element) combine) {
+    return _states.fold(initialValue, combine);
+  }
+
+  @override
+  Future<void> forEach(void Function(State element) action) {
+    return _states.forEach(action);
+  }
+
+  @override
+  Stream<State> handleError(Function onError, {bool Function(dynamic error)? test}) {
+    return _states.handleError(onError, test: test);
+  }
+
+  @override
+  bool get hasError => _states.hasError;
+
+  @override
+  bool get hasListener => _states.hasListener;
+
+  @override
+  bool get hasValue => _states.hasValue;
+
+  @override
+  bool get isBroadcast => _states.isBroadcast;
+
+  @override
+  bool get isClosed => _states.isClosed;
+
+  @override
+  Future<bool> get isEmpty => _states.isEmpty;
+
+  @override
+  bool get isPaused => _states.isPaused;
+
+  @override
+  Future<String> join([String separator = ""]) {
+    return _states.join(separator);
+  }
+
+  @override
+  Future<State> get last => _states.last;
+
+  @override
+  StreamNotification<State>? get lastEventOrNull => _states.lastEventOrNull;
+
+  @override
+  Future<State> lastWhere(bool Function(State element) test, {State Function()? orElse}) {
+    return _states.lastWhere(test, orElse: orElse);
+  }
+
+  @override
+  Future<int> get length => _states.length;
+
+  @override
+  StreamSubscription<State> listen(void Function(State value)? onData,
+      {Function? onError, void Function()? onDone, bool? cancelOnError}) {
+    return _states.listen(onData, onError: onError, onDone: onDone, cancelOnError: cancelOnError);
+  }
+
+  @override
+  Stream<S> map<S>(S Function(State event) convert) {
+    return _states.map(convert);
+  }
+
+  @override
+  void onAdd(State event) {
+    _states.onAdd(event);
+  }
+
+  @override
+  void onAddError(Object error, [StackTrace? stackTrace]) {
+    _states.onAddError(error, stackTrace);
+  }
+
+  @override
+  Future pipe(StreamConsumer<State> streamConsumer) {
+    return _states.pipe(streamConsumer);
+  }
+
+  @override
+  Future<State> reduce(State Function(State previous, State element) combine) {
+    return _states.reduce(combine);
+  }
+
+  @override
+  Future<State> get single => _states.single;
+
+  @override
+  Future<State> singleWhere(bool Function(State element) test, {State Function()? orElse}) {
+    return _states.singleWhere(test, orElse: orElse);
+  }
+
+  @override
+  StreamSink<State> get sink => _states.sink;
+
+  @override
+  Stream<State> skip(int count) {
+    return _states.skip(count);
+  }
+
+  @override
+  Stream<State> skipWhile(bool Function(State element) test) {
+    return _states.skipWhile(test);
+  }
+
+  @override
+  StackTrace? get stackTrace => _states.stackTrace;
+
+  @override
+  ValueStream<State> get stream => _states.stream;
+
+  @override
+  Stream<State> take(int count) {
+    return _states.take(count);
+  }
+
+  @override
+  Stream<State> takeWhile(bool Function(State element) test) {
+    return _states.takeWhile(test);
+  }
+
+  @override
+  Stream<State> timeout(Duration timeLimit, {void Function(EventSink<State> sink)? onTimeout}) {
+    return _states.timeout(timeLimit, onTimeout: onTimeout);
+  }
+
+  @override
+  Future<List<State>> toList() {
+    return _states.toList();
+  }
+
+  @override
+  Future<Set<State>> toSet() {
+    return _states.toSet();
+  }
+
+  @override
+  Stream<S> transform<S>(StreamTransformer<State, S> streamTransformer) {
+    return _states.transform(streamTransformer);
+  }
+
+  @override
+  State? get valueOrNull => _states.valueOrNull;
+
+  @override
+  Stream<State> where(bool Function(State event) test) {
+    return _states.where(test);
+  }
+}
