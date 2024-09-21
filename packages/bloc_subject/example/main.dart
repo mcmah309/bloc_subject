8ce44bea4dd85b98bc206b185d26f1f2a71e79df
mcmah309 <dillonmcmahon79@gmail.com>
2024-09-21 15:10:09 +0000

    doc: Update example

--- packages/bloc_subject/example/main.dart
+++ packages/bloc_subject/example/main.dart
@@ -1,12 +1,22 @@
 import 'package:bloc_subject/bloc_subject.dart';
 
-sealed class AlphabetState {}
+sealed class AlphabetState {
+  final int id;
 
-class A implements AlphabetState {}
+  AlphabetState(this.id);
+}
 
-class B implements AlphabetState {}
+class A extends AlphabetState {
+  A(super.id);
+}
 
-class C implements AlphabetState {}
+class B extends AlphabetState {
+  B(super.id);
+}
+
+class C extends AlphabetState {
+  C(super.id);
+}
 
 sealed class AlphabetEvent {}
 
@@ -17,34 +27,35 @@ class Y implements AlphabetEvent {}
 class Z implements AlphabetEvent {}
 
 void main() async {
-  BlocSubject<AlphabetEvent, AlphabetState> subject = BlocSubject.fromValue(A(),
+  int emitCount = 0;
+  BlocSubject<AlphabetEvent, AlphabetState> subject = BlocSubject.fromValue(A(emitCount),
       handler: (event, state) => switch (event) {
-            X() => A(),
-            Y() => B(),
+            X() => A(++emitCount),
+            Y() => B(++emitCount),
             Z() => null,
           });
   final transformedStream = subject.stream
       .map((value) => switch (value) {
-            A() => "A",
-            B() => "B",
-            C() => "C",
+            A() => "A${value.id}",
+            B() => "B${value.id}",
+            C() => "C${value.id}",
           })
       .distinct();
 
   assert(subject.value is A);
-  assert(await transformedStream.first == "A");
+  assert(await transformedStream.first == "A0");
 
   subject.addEvent(Y()); // Can process events and emit new states
   await Future.delayed(const Duration(milliseconds: 100));
   assert(subject.value is B);
-  assert(await transformedStream.first == "B");
+  assert(await transformedStream.first == "B1");
 
-  subject.addEvent(Z()); // If null is emitted from the handler, nothing is emitted
+  subject.addEvent(Z()); // If null is emitted from the handler, the state does not change/emit
   await Future.delayed(const Duration(milliseconds: 100));
   assert(subject.value is B);
-  assert(await transformedStream.first == "B");
+  assert(await transformedStream.first == "B1");
 
-  subject.add(C()); // Still works like a regular `BehaviorSubject`
+  subject.add(C(1000)); // Still works like a regular `BehaviorSubject`
   assert(subject.value is C);
-  assert(await transformedStream.first == "C");
+  assert(await transformedStream.first == "C1000");
 }
